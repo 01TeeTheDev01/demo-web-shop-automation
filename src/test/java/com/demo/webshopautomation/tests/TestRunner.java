ad320bb4b31133af3cf7ec44982587b097815391
Teethedev@outlook.com <Teethedev@outlook.com>
2024-07-23 20:34:30 +0200

    Updated TestRunner with correct report plugin.

--- src/test/java/com/demo/webshopautomation/tests/TestRunner.java
+++ src/test/java/com/demo/webshopautomation/tests/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/com/demo/webshopautomation/features",
         glue = "steps",
-        plugin = {"com.aventstack.extentreports:html"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         publish = true)
 public class TestRunner extends AbstractTestNGCucumberTests { }
